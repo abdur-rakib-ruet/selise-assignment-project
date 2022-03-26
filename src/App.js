33fcb0be2881e2811b44014306f7978b91b09fa4
Abdur Rakib <abdurrakib.cse.ruet@gmail.com>
2022-03-26 09:54:15 +0600

    Initialized project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Typography } from "antd";
+import "./App.css";
+
+const { Text } = Typography;
+
+const App = () => {
+  return (
+    <div>
+      <Text type='success'>Selise Assignment Project</Text>
+    </div>
+  );
+};
+
+export default App;
